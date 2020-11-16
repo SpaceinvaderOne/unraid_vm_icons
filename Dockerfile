b540e0ffc35f72a9c6348186c93926013212238f
SpaceinvaderOne <ed@iMac-2.local>
2020-11-16 00:31:52 +0000

    fix

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN apt-get update && apt-get -y install wget bash rsync
 COPY . /iconsync
 VOLUME /unraid_vm_icons
 VOLUME /icons
-CMD ./iconsync/icon_download.sh || : && bash && tail -f /dev/null
+CMD bash ./iconsync/icon_download.sh || : && bash && tail -f /dev/null
--- icon_download.sh
+++ icon_download.sh
@@ -8,14 +8,14 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Directory for downloaded icons to be stored
-DIR="/icons/icons"
+DIR="/config/icons"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # #  Functions                                                          # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Delete icon store if present and delete is set to yes in template
-deletedirectories() {
+shall_i_delete() {
 	if [ -d $DIR ] && [ $delete == "yes" ]; then
 		
 	rm -r $DIR
@@ -24,20 +24,23 @@ deletedirectories() {
     echo "."
 else
 	echo "  ......continuing."
-    echo "."
-    echo "."
 
-	fi			
-		
+	fi					
 
 }
 
+
 # Create icon directory if not present if directory exists sync contents to vm manger and exit
-createorsync() {
+download_or_sync() {
 	if [ ! -d $DIR ] ; then
 		
 	mkdir -vp $DIR
-	echo "I have created the icon directories now will start downloading selected icons"
+	echo "I have created the icon store directory & now will start downloading selected icons"
+	
+	if [ -d /config/unraid_vm_icons ] ; then
+    rm -r /config/unraid_vm_icons
+    fi
+	git -C /config clone https://github.com/SpaceinvaderOne/unraid_vm_icons.git
 	downloadwindows
 	downloadlinux
 	downloadfreebsd
@@ -47,13 +50,14 @@ createorsync() {
     echo "."
 	echo "icons downloaded and synced"
 else
-	rsync -a $DIR/* /unraid_vm_icons
+	
     echo "."
     echo "."
 	echo "Icons downloaded previously."
 	echo "icons synced"
 
-	fi			
+	fi	
+	rsync -a $DIR/* /unraid_vm_icons		
 		
 
 }
@@ -61,8 +65,7 @@ else
 # Download windows based OS icons if set in template
 downloadwindows() {
     if [ $windows == "yes" ] ; then
-	get="-r -c -S -N -nH -e robots=off -nd -np -A png -R index.html* https://raw.githubusercontent.com/SpaceinvaderOne/unraid_vm_icons/master/icons/Windows/"
-    wget $get -P $DIR
+	rsync -a /config/unraid_vm_icons/icons/Windows/* $DIR
 			else
 				echo "  windows based os icons not wanted......continuing."
 			    echo "."
@@ -75,9 +78,7 @@ fi
 # Download linux based OS icons if set in template
 downloadlinux() {
     if [ $linux == "yes" ] ; then
-	#set wget to download all linux based os icons
-	get2="-r -c -S -N -nH -e robots=off -nd -np -A png -R index.html* https://raw.githubusercontent.com/SpaceinvaderOne/unraid_vm_icons/master/icons/Linux/"
-    wget $get2 -P $DIR
+    rsync -a /config/unraid_vm_icons/icons/Linux/* $DIR
 			else
 				echo "  linux based os icons not wanted......continuing."
 			    echo "."
@@ -91,9 +92,7 @@ fi
 # Download freebsd based OS icons if set in template
 downloadfreebsd() {
     if [ $freebsd == "yes" ] ; then
-	#set wget to download all freebsd based os icons
-	get3="-r -c -S -N -nH -e robots=off -nd -np -A png -R index.html* https://raw.githubusercontent.com/SpaceinvaderOne/unraid_vm_icons/master/icons/Freebsd/"
-    wget $get3 -P $DIR
+	rsync -a /config/unraid_vm_icons/icons/Freebsd/* $DIR
 			else
 				echo "  freebsd based os icons not wanted......continuing."
 			    echo "."
@@ -106,9 +105,7 @@ fi
 # Download other OS icons if set in template
 downloadother() {
     if [ $other == "yes" ] ; then
-	#set wget to download all other os icons
-	get4="-r -c -S -N -nH -e robots=off -nd -np -A png -R index.html* https://raw.githubusercontent.com/SpaceinvaderOne/unraid_vm_icons/master/icons/Other/"
-    wget $get4 -P $DIR
+	rsync -a /config/unraid_vm_icons/icons/Other/* $DIR
 			else
 				echo "  other os icons not wanted......continuing."
 			    echo "."
@@ -121,9 +118,7 @@ fi
 # Download macOS based OS icons if set in template
 downloadmacos() {
     if [ $macos == "yes" ] ; then
-	#set wget to download all windows based os icons
-	get5="-r -c -S -N -nH -e robots=off -nd -np -A png -R index.html* https://raw.githubusercontent.com/SpaceinvaderOne/unraid_vm_icons/master/icons/macOS/"
-    wget $get5 -P $DIR
+	rsync -a /config/unraid_vm_icons/icons/macOS/* $DIR
 			else
 				echo "  macos based os icons not wanted......continuing."
 			    echo "."
@@ -134,8 +129,7 @@ fi
 }
 
 
-
-deletedirectories
-createorsync
+shall_i_delete
+download_or_sync
 
 
